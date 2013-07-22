9c7b0c1c0c69a17bbf086b491b7345c7684559ef
zsyed <zulfiqarsyed@yahoo.com>
2013-07-21 17:49:00 -0700

    uploading database files.

---
+++ MvcApplication1/MvcApplication1/Infrastructure/DepartmentDb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using eManager.Domain;
+
+namespace MvcApplication1.Infrastructure
+{
+    public class DepartmentDb : DbContext, IDepartmentDataSource
+    {
+        public DepartmentDb(): base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        IQueryable<Employee> IDepartmentDataSource.Employees
+        {
+            get { return Employees; }
+        }
+
+        IQueryable<Department> IDepartmentDataSource.Departments
+        {
+            get { return Departments; }
+        }
+    }
+}
