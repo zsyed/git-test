12f916205721a06cdcb36da3995bd52f7bd80134
zsyed <zulfiqarsyed@yahoo.com>
2013-07-28 18:48:46 -0700

    database setinitializer

--- MvcApplication1/MvcApplication1/Global.asax.cs
+++ MvcApplication1/MvcApplication1/Global.asax.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using MvcApplication1.Infrastructure;
 
 namespace MvcApplication1
 {
@@ -16,6 +18,9 @@ namespace MvcApplication1
     {
         protected void Application_Start()
         {
+
+            Database.SetInitializer<DepartmentDb>(null);
+
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
